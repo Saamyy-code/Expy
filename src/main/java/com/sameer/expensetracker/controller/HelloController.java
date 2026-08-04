10e9b6ab395f4057aff4decd61a47797b7435534
Sameer Khan <sammfffyy@gmail.com>
2026-08-04 17:33:17 +0530

    Implement constructor dependency injection

--- src/main/java/com/sameer/expensetracker/controller/HelloController.java
+++ src/main/java/com/sameer/expensetracker/controller/HelloController.java
@@ -1,12 +1,18 @@
 package com.sameer.expensetracker.controller;//Tells Java this class belongs to the controller package
 
+import com.sameer.expensetracker.service.GreetingService;
 import org.springframework.web.bind.annotation.GetMapping;// Imports SpringBoot Classes
 import org.springframework.web.bind.annotation.RestController;// Imports SpringBoot Classes
 
-@RestController //This class contains REST API endpoints
+@RestController
 public class HelloController {
+    private GreetingService greetingService;
+
+    public HelloController(GreetingService greetingService){
+            this.greetingService = greetingService;
+    }
     @GetMapping("/hello")
     public String sayHello(){
-        return "Welcome to Smart Expense Tracker!";
+        return greetingService.getGreeting();
     }
 }
---
+++ src/main/java/com/sameer/expensetracker/service/GreetingService.java
@@ -0,0 +1,10 @@
+package com.sameer.expensetracker.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+    public String getGreeting(){
+        return "Welcome to Smart Expense Tracker!";
+    }
+}
